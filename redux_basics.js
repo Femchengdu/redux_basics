5136f8c40a89ff5ce8ebd20585ff600a1deab9fb
Femchengdu <femiajayi705@gmail.com>
2018-12-31 16:18:35 +0000

    Initialized a reuducer

--- redux_basics.js
+++ redux_basics.js
@@ -1,6 +1,19 @@
-// Store
+const redux = require('redux');
+const create_a_store = redux.createStore
+
+//
+const initialize_reducer = {
+	counter: 0
+}
 
 // Reducer
+const appRecuder = (state = initialize_reducer, action) => {
+	return state;
+}
+
+// Store
+const store = create_a_store(appRecuder);
+console.log(store.getState());
 
 // Dispatching Action
 
