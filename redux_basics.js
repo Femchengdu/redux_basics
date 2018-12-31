05918a9cdc2a59324c195895087782b957b2cdc8
Femchengdu <femiajayi705@gmail.com>
2018-12-31 16:47:58 +0000

    Added basic dispatch actions

--- redux_basics.js
+++ redux_basics.js
@@ -8,6 +8,19 @@ const initialize_reducer = {
 
 // Reducer
 const appRecuder = (state = initialize_reducer, action) => {
+	if (action.type === 'INCREMENT_COUNTER') {
+		return {
+			...state,
+			counter: state.counter + 1
+		}
+	}
+
+	if (action.type === 'ADD_VAL_TO_COUNTER') {
+		return {
+			...state,
+			counter: state.counter + action.value
+		}
+	}
 	return state;
 }
 
@@ -16,5 +29,8 @@ const store = create_a_store(appRecuder);
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({type: 'INCREMENT_COUNTER'});
+store.dispatch({type: 'ADD_VAL_TO_COUNTER', value: 10});
+console.log(store.getState());
 
 // Subscription
