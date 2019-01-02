701e99da8a906a4a1a0c53c74689a2409649df86
Femchengdu <femiajayi705@gmail.com>
2019-01-02 17:17:00 +0000

    Restructured the reducer using a switch statement

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,34 +1,31 @@
-//..
+//....
 const initialize_reducer = {
 	counter: 0
 }
 
 const reducer = (state = initialize_reducer, action) => {
-	// Increment
-	if (action.type === 'INCREMENT') {
-		return {
-			counter: state.counter + 1
-		}
+	// Using a swithch statement
+
+	switch (action.type) {
+		case 'INCREMENT':
+			return {
+				counter: state.counter + 1
+			}
+		case 'DECREMENT':
+			return {
+				counter: state.counter - 1
+			}
+		case 'ADD':
+			return {
+				counter: state.counter + action.value
+			}
+		case 'SUBTRACT':
+			return {
+				counter: state.counter - action.value
+			}
+		default:
+			return state;
 	}
-	// Decrement
-	if (action.type === 'DECREMENT') {
-		return {
-			counter: state.counter - 1
-		}
-	}
-	// Add
-	if (action.type === 'ADD') {
-		return {
-			counter: state.counter + action.value
-		}
-	}
-	// Subtract
-	if (action.type === 'SUBTRACT') {
-		return {
-			counter: state.counter - action.value
-		}
-	}
-	return state;
 }
 
 export default reducer;
