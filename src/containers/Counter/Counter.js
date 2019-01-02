9647063e28185259ca957c4a4f827563f67107f2
Femchengdu <femiajayi705@gmail.com>
2019-01-02 15:34:58 +0000

    Added more dispatch actions

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,39 +1,19 @@
-//...
+//.......
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
 
     render () {
         return (
             <div>
                 <CounterOutput value={this.props.redux_state_counter} />
                 <CounterControl label="Increment" clicked={this.props.increment_the_counter} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Decrement" clicked={this.props.decrement_the_counter}  />
+                <CounterControl label="Add 5" clicked={this.props.counter_plus_5}  />
+                <CounterControl label="Subtract 5" clicked={this.props.counter_minus_5}  />
             </div>
         );
     }
@@ -51,7 +31,10 @@ const stateToProps = state => {
 // React redux also provides the dispatch helper variable
 const dispatchToProps = dispatch => {
     return {
-        increment_the_counter: () => dispatch({type: 'INCREMENT'})
+        increment_the_counter: () => dispatch({type: 'INCREMENT'}),
+        decrement_the_counter: () => dispatch({type: 'DECREMENT'}),
+        counter_minus_5: () => dispatch({type: 'SUBTRACT', value: 5}),
+        counter_plus_5: () => dispatch({type: 'ADD', value: 5})
     }
 }
 // connect is a function that returns a function which takes a component as input
