386a4bc5091580aa975f8ebcfdb07a43c04d97fa
Femchengdu <femiajayi705@gmail.com>
2019-01-03 16:30:27 +0000

    Modified the reducer to update the state immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,4 +1,4 @@
-//.......
+//........
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
@@ -14,6 +14,16 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.decrement_the_counter}  />
                 <CounterControl label="Add 5" clicked={this.props.counter_plus_5}  />
                 <CounterControl label="Subtract 5" clicked={this.props.counter_minus_5}  />
+                <hr />
+                <button onClick={this.props.store_result}>Store Result</button>
+                <ul>
+                    {
+                        this.props.stored_result.map(result => (
+                            <li key={result.id} onClick={this.props.delete_result}>{result.value}</li>
+                        ))
+                    
+                    }
+                </ul>
             </div>
         );
     }
@@ -24,7 +34,8 @@ class Counter extends Component {
 // React redux provide the state helper variabl
 const stateToProps = state => {
     return {
-        redux_state_counter: state.counter
+        redux_state_counter: state.counter,
+        stored_result: state.results
     }
 }
 
@@ -34,7 +45,9 @@ const dispatchToProps = dispatch => {
         increment_the_counter: () => dispatch({type: 'INCREMENT'}),
         decrement_the_counter: () => dispatch({type: 'DECREMENT'}),
         counter_minus_5: () => dispatch({type: 'SUBTRACT', value: 5}),
-        counter_plus_5: () => dispatch({type: 'ADD', value: 5})
+        counter_plus_5: () => dispatch({type: 'ADD', value: 5}),
+        store_result: () => dispatch({type: 'STORE_RESULT'}),
+        delete_result: () => dispatch({type: 'DELETE_RESULT'})
     }
 }
 // connect is a function that returns a function which takes a component as input
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,7 @@
 //....
 const initialize_reducer = {
-	counter: 0
+	counter: 0,
+	results: []
 }
 
 const reducer = (state = initialize_reducer, action) => {
@@ -8,20 +9,32 @@ const reducer = (state = initialize_reducer, action) => {
 
 	switch (action.type) {
 		case 'INCREMENT':
-			return {
-				counter: state.counter + 1
-			}
+		// Change the state immutably
+		const newState = Object.assign({}, state);
+		//console.log('[State Counter:]', state.counter);
+		newState.counter = state.counter + 1;
+		//console.log('[newState Counter:]', newState.counter);
+		//console.log('[State Counter After:]', state.counter);
+		return newState;
 		case 'DECREMENT':
 			return {
+				...state,
 				counter: state.counter - 1
 			}
 		case 'ADD':
 			return {
+				...state,
 				counter: state.counter + action.value
 			}
 		case 'SUBTRACT':
 			return {
+				...state,
 				counter: state.counter - action.value
+			}
+		case 'STORE_RESULT':
+			return {
+				...state,
+				results: state.results.concat({id: new Date(), value: state.counter})
 			}
 		default:
 			return state;
