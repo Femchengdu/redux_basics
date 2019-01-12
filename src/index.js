35851a8dac3206029add554b4a32fe39457f7397
Femchengdu <femiajayi705@gmail.com>
2019-01-12 17:13:25 +0000

    Added a basic middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import counterReducer from './store/reducers/counter_reducer';
 import resultReducer from './store/reducers/result_reducer';
 import './index.css';
@@ -13,7 +13,18 @@ const rootReducer = combineReducers({
 	res_reducer: resultReducer
 });
 
-const store = createStore(rootReducer)
+const middleware_logger = store => {
+	return proceed_to_reducer => {
+		return action => {
+			console.log('[middlewareLogger] Sending', action);
+			const terminate_recusive_call = proceed_to_reducer(action);
+			console.log('[middlewareLogger] accessing store:', store.getState());
+			return terminate_recusive_call;
+		}
+	}
+}
+
+const store = createStore(rootReducer, applyMiddleware(middleware_logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
